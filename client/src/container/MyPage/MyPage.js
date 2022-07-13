51bf585e09a146bd373ecbff195d0f3f00564cf3
Jonghan Park <jonghan.park@edu.sait.ca>
2022-07-12 23:07:29 -0600

    Update global variable using useContext

--- client/src/container/MyPage/MyPage.js
+++ client/src/container/MyPage/MyPage.js
@@ -1,9 +1,11 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
+import { UserContext } from "../../App";
 import axios from "axios";
 
-function MyPage({ user }) {
-  console.log(user);
-  return <h1>ddd</h1>;
+function MyPage() {
+  const username = useContext(UserContext);
+
+  return <h2>{username.user.name}</h2>;
 }
 
 export default MyPage;
