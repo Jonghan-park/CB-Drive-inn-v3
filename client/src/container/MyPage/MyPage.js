fbc208338351a54086cfc42eab60737fae1a68c6
Jonghan Park <jonghan.park@edu.sait.ca>
2022-07-13 11:01:32 -0600

    Update my page

--- client/src/container/MyPage/MyPage.js
+++ client/src/container/MyPage/MyPage.js
@@ -1,11 +1,28 @@
 import React, { useState, useEffect, useContext } from "react";
 import { UserContext } from "../../App";
 import axios from "axios";
+import "./MyPage.css";
 
 function MyPage() {
-  const username = useContext(UserContext);
+  const user = useContext(UserContext);
 
-  return <h2>{username.user.name}</h2>;
+  return (
+    <div className="myPage">
+      <h3 className="myPageTitle">My Page</h3>
+      <div className="underline" />
+      <form className="myPage-form">
+        <h2>Hello, {user.user.name}!</h2>
+        <h3>Your Email:</h3>
+        <h4>{user.user.email}</h4>
+        <div className="myPagePic">
+          <img src={user.user.pic} alt="avatar in my page" />
+        </div>
+        <button type="submit" className="submit">
+          Change the profile
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export default MyPage;
