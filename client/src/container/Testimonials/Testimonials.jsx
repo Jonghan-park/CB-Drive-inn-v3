2b33307a3287f3fb5156c7e85854e7b56a8439d6
Jonghan Park <jonghan.park@edu.sait.ca>
2022-07-24 19:49:06 -0600

    Update rating

--- client/src/container/Testimonials/Testimonials.jsx
+++ client/src/container/Testimonials/Testimonials.jsx
@@ -1,6 +1,7 @@
-import React from 'react'
-import './Testimonials.css'
-import reviews from '../../data/testimonials'
+import React from "react";
+import "./Testimonials.css";
+import reviews from "../../data/testimonials";
+import Rating from "../Rating/Rating";
 
 const Testimonials = () => {
   return (
@@ -9,20 +10,23 @@ const Testimonials = () => {
         <h1>Testimonials</h1>
         <div className="underline" />
       </div>
-      
+
       <div className="testimonials">
-        {reviews.map((testimonial)=>{
-          const {id, name, stars, review, img} = testimonial;
-          return <div className="testimonial" key={id}>
-            <img src={img} alt={name} />
-            <h3 className="name">{name}</h3>
-            <div className="stars">{stars}</div>
-            <p>{review}</p>
-          </div>
+        {reviews.map((testimonial) => {
+          const { id, name, stars, review, img } = testimonial;
+          return (
+            <div className="testimonial" key={id}>
+              <img src={img} alt={name} />
+              <h3 className="name">{name}</h3>
+              <Rating className="stars" stars={stars} />
+              {/* <div className="stars">{stars}</div> */}
+              <p>{review}</p>
+            </div>
+          );
         })}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Testimonials
+export default Testimonials;
