c8a50192db9465ca897c0b15e38a10746bec4290
Jonghan Park <jonghan.park@edu.sait.ca>
2022-08-02 17:02:20 -0600

    make it better:

--- server/Database/db.js
+++ server/Database/db.js
@@ -5,7 +5,7 @@ module.exports = () => {
     useUnifiedTopology: true,
   };
   try {
-    mongoose.connect(process.env.DB, connectionParams);
+    mongoose.connect(process.env.MONGO_URI, connectionParams);
     console.log("Connected to database successfully");
   } catch (error) {
     console.log(error);
--- server/server.js
+++ server/server.js
@@ -50,9 +50,9 @@ app.use("/order", orderRoute);
 app.use("/get", productRoute);
 
 // const __dirname = path.resolve();
-app.use(express.static(path.join(__dirname, "client/build")));
+app.use(express.static("client/build"));
 app.get("*", (req, res) =>
-  res.sendFile(path.join(__dirname, "client/build/index.html"))
+  res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"))
 );
 
 app.listen(port, () => {
