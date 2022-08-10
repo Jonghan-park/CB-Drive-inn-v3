d802d4b2d634d661453f92721cdc12c5a56f3269
Jonghan Park <jonghan.park@edu.sait.ca>
2022-08-10 00:46:33 -0600

    Update image

--- server/client/src/data/slider-data.js
+++ server/client/src/data/slider-data.js
@@ -1,22 +1,16 @@
-import img1 from '../images/main2.jpeg'
-import img2 from '../images/main1.jpeg'
-import img3 from '../images/main3.jpeg'
-
+import img1 from "../images/main1.png";
+import img2 from "../images/main2.png";
+import img3 from "../images/main3.jpeg";
 
 export const data = [
   {
-    image: img1, 
-    heading: "Calgary Best Hamburger",
-    desc: "This is good",
+    image: img1,
+    heading: "Sweet Chili Chichen Burger",
+    desc: "Chicken burger with sweet chili sauce",
   },
   {
-    image: img2, 
-    heading: "Best Milkshake",
-    desc: "This is good2",
+    image: img2,
+    heading: "Deep Fried Pickle",
+    desc: "Crispy and Juicy fried pickle",
   },
-  {
-    image: img3, 
-    heading: "Deep fried pickle",
-    desc: "Our new menu",
-  },
-]
+];
