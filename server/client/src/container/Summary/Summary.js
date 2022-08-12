e5c59db7e8e36039c884d2d47af8e1b8f0709004
Jonghan Park <jonghan.park@edu.sait.ca>
2022-08-11 22:03:44 -0600

    Update summary

--- server/client/src/container/Summary/Summary.js
+++ server/client/src/container/Summary/Summary.js
@@ -6,26 +6,46 @@ const Summary = () => {
   const cartCtx = useContext(CartContext);
   const { items, totalAmount } = cartCtx;
 
+  const plusTaxTotalAmount = () => {
+    const tax = 5;
+    const totalTax = totalAmount * (tax / 100);
+    const plusTaxAmount = totalTax + totalAmount;
+    return plusTaxAmount.toFixed(2);
+  };
+
   return (
-    <div className="summary">
+    <div>
       <h3 className="summaryTitle">Order Summary</h3>
       <div className="underline" />
-      <div className="order_summary_container">
-        {items.length === 0 && <h2>No items</h2>}
-        {items &&
-          items.map((item) => {
-            const { id, title, amount, price, img } = item;
-            return (
-              <div className="eachItem" key={id}>
-                <img src={img} />
-                <div className="">
+      <div className="summary">
+        <div className="order_summary_container_left">
+          {items.length === 0 && <h2>No items</h2>}
+          {items &&
+            items.map((item) => {
+              const { id, title, amount, price, img } = item;
+              return (
+                <div className="eachItem" key={id}>
+                  <img src={img} />
+
                   <div className="summary_title">{title}</div>
                   <div className="summary_amount">{amount}</div>
                   <div className="summary_price">{price}</div>
                 </div>
-              </div>
-            );
-          })}
+              );
+            })}
+
+          <div className="summary_checkout">
+            <hr className="summary-line" />
+            <div className="subtotal">Sub-Total</div>
+            <div className="total-amount">${plusTaxTotalAmount()}</div>
+          </div>
+        </div>
+        <div className="order_summary_container_right">
+          <form action="">
+            <label htmlFor="">dddd</label>
+            <input type="text" />
+          </form>
+        </div>
       </div>
     </div>
   );
