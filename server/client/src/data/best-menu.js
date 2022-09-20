73763c5f8c90f9874c9174ad178a03b9742c209a
Jonghan Park <jonghan.park@edu.sait.ca>
2022-09-19 20:08:19 -0600

    Update pics

--- server/client/src/data/best-menu.js
+++ server/client/src/data/best-menu.js
@@ -1,31 +1,32 @@
-import img1 from '../images/item-8.jpeg'
-import img2 from '../images/item-6.jpeg'
-import img3 from '../images/item-2.jpeg'
+import mushroomSwissBurger from "../images/mushroom_swiss_burger.png";
+import sweetChiliChickenBurger from "../images/sweet_chili_chicken_burger.png";
+import doubleBurger from "../images/double_burger.png";
+
 const bestMenu = [
   {
     id: 1,
-    title: 'Mushroom Swiss',
-    category: 'Special Burgers',
+    title: "Mushroom Swiss",
+    category: "Special Burgers",
     price: 6.75,
-    img: img1,
+    img: mushroomSwissBurger,
     desc: `on it tumblr kickstarter thundercats migas everyday carry squid palo santo leggings. Food truck truffaut  `,
   },
   {
     id: 2,
-    title: 'Sweet Chili Chicken Burger',
-    category: 'Special Burgers',
-    price: 6.50,
-    img: img2,
+    title: "Sweet Chili Chicken Burger",
+    category: "Special Burgers",
+    price: 6.5,
+    img: sweetChiliChickenBurger,
     desc: `Portland chicharrones ethical edison bulb, palo santo craft beer chia heirloom iPhone everyday`,
   },
   {
     id: 3,
-    title: 'Master(Double patties)',
-    category: 'Burgers',
-    price: 5.80,
-    img: img3,
+    title: "Master(Double patties)",
+    category: "Burgers",
+    price: 5.8,
+    img: doubleBurger,
     desc: `vaporware iPhone mumblecore selvage raw denim slow-carb leggings gochujang helvetica man braid jianbing. Marfa thundercats `,
   },
-]
+];
 
-export default bestMenu
+export default bestMenu;
--- server/client/src/data/data.js
+++ server/client/src/data/data.js
@@ -1,13 +1,51 @@
-import img1 from "../images/item-1.jpeg";
-import img2 from "../images/item-2.jpeg";
-import img3 from "../images/item-3.jpeg";
-import img4 from "../images/item-4.jpeg";
-import img5 from "../images/item-5.jpeg";
-import img6 from "../images/item-6.jpeg";
-import img7 from "../images/item-7.jpeg";
-import img8 from "../images/item-8.jpeg";
-import img9 from "../images/item-9.jpeg";
-import img10 from "../images/item-10.jpeg";
+import regularBurger from "../images/regular_burger.png";
+import doubleBurger from "../images/double_burger.png";
+import tripleBurger from "../images/triple_burger.png";
+import quadrupleBurger from "../images/quadruple_burger.png";
+import bacon from "../images/bacon.png";
+import mushroomSwiss from "../images/mushroom_swiss.png";
+import cheese from "../images/cheese.png";
+import chili from "../images/chili.png";
+import backbacon from "../images/backbacon.png";
+import fishBurger from "../images/fish_burger.png";
+import chickenBurger from "../images/chicken_burger.png";
+import sweetChiliChickenBurger from "../images/sweet_chili_chicken_burger.png";
+import veggieBurger from "../images/vegie_burger.png";
+import mushroomSwissBurger from "../images/mushroom_swiss_burger.png";
+import chickenFingerDinner from "../images/chicken_finger_dinner.png";
+import shrimpDinner from "../images/shrimp_dinner.png";
+import fishAndChips from "../images/fish_and_chips.png";
+import regularFries from "../images/regular_fries.png";
+import familyFries from "../images/family_fries.png";
+import poutine from "../images/poutine.png";
+import chiliCheeseFries from "../images/chili_cheese_fries.png";
+import regularOnionRings from "../images/regular_onion_rings.png";
+import familyOnionRings from "../images/family_onion_rings.png";
+import chickenFingers from "../images/chicken_fingers.png";
+import chickenNuggets from "../images/chicken_nuggets.png";
+import chickenNuggets10 from "../images/chicken_nuggets_10pc.png";
+import zucchiniSticks from "../images/zucchini_sticks.png";
+import deepFriedMushrooms from "../images/deep_fried_mushroom.png";
+import pizzaPop from "../images/pizza_pop.png";
+import cornDog from "../images/corn_dog.png";
+import applePie from "../images/apple_pie.png";
+import deepFriedPickle from "../images/deep_fried_pickle.png";
+import gravy from "../images/gravy.png";
+import cupOfChili from "../images/cup_of_chili.png";
+import garlicMayo from "../images/garlic_mayo.png";
+import hotDog from "../images/hot_dog.png";
+import chiliDog from "../images/chili_dog.png";
+import cheeseDog from "../images/cheese_dog.png";
+import chiliCheeseDog from "../images/chili_cheese_dog.png";
+import pop from "../images/pop.png";
+import slush from "../images/slush.png";
+import milkShakes from "../images/milkshake.png";
+import float from "../images/float.png";
+import screamers from "../images/screamer.png";
+import whiteMilk from "../images/milk.png";
+import chocoMilk from "../images/chocolate_milk.png";
+import coffee from "../images/coffee.png";
+import hotChoco from "../images/hot_choco.png";
 
 const menu = [
   {
@@ -15,7 +53,7 @@ const menu = [
     title: "Regular(Single patty)",
     category: "Burgers",
     price: 4.35,
-    img: img1,
+    img: regularBurger,
     InStock: true,
     desc: `I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed `,
   },
@@ -24,7 +62,7 @@ const menu = [
     title: "Master(Double patties)",
     category: "Burgers",
     price: 5.8,
-    img: img2,
+    img: doubleBurger,
     InStock: true,
     desc: `vaporware iPhone mumblecore selvage raw denim slow-carb leggings gochujang helvetica man braid jianbing. Marfa thundercats `,
   },
@@ -33,7 +71,7 @@ const menu = [
     title: "Centenniel(Triple patties)",
     category: "Burgers",
     price: 7.2,
-    img: img3,
+    img: tripleBurger,
     InStock: true,
     desc: `ombucha chillwave fanny pack 3 wolf moon street art photo booth before they sold out organic viral.`,
   },
@@ -42,16 +80,25 @@ const menu = [
     title: "Monster(Quadriple patties)",
     category: "Burgers",
     price: 8.55,
-    img: img4,
+    img: quadrupleBurger,
     InStock: true,
     desc: `Shabby chic keffiyeh neutra snackwave pork belly shoreditch. Prism austin mlkshk truffaut, `,
   },
   {
     id: 5,
-    title: "Bacon or Mushrooom",
+    title: "Bacon",
     category: "Extra Toppings",
     price: 1.5,
-    img: img5,
+    img: bacon,
+    InStock: true,
+    desc: `Shabby chic keffiyeh neutra snackwave pork belly shoreditch. Prism austin mlkshk truffaut, `,
+  },
+  {
+    id: 50,
+    title: "Mushrooom swiss",
+    category: "Extra Toppings",
+    price: 1.5,
+    img: mushroomSwiss,
     InStock: true,
     desc: `Shabby chic keffiyeh neutra snackwave pork belly shoreditch. Prism austin mlkshk truffaut, `,
   },
@@ -60,7 +107,7 @@ const menu = [
     title: "Cheese",
     category: "Extra Toppings",
     price: 0.75,
-    img: img7,
+    img: cheese,
     InStock: true,
     desc: `Shabby chic keffiyeh neutra snackwave pork belly shoreditch. Prism austin mlkshk truffaut, `,
   },
@@ -69,7 +116,7 @@ const menu = [
     title: "Chili",
     category: "Extra Toppings",
     price: 1.95,
-    img: img8,
+    img: chili,
     InStock: true,
     desc: `Shabby chic keffiyeh neutra snackwave pork belly shoreditch. Prism austin mlkshk truffaut, `,
   },
@@ -78,7 +125,7 @@ const menu = [
     title: "Backbacon",
     category: "Extra Toppings",
     price: 2.25,
-    img: img10,
+    img: backbacon,
     InStock: true,
     desc: `Shabby chic keffiyeh neutra snackwave pork belly shoreditch. Prism austin mlkshk truffaut, `,
   },
@@ -87,7 +134,7 @@ const menu = [
     title: "Fish Burger",
     category: "Special Burgers",
     price: 6.25,
-    img: img5,
+    img: fishBurger,
     InStock: true,
     desc: `franzen vegan pabst bicycle rights kickstarter pinterest meditation farm-to-table 90's pop-up `,
   },
@@ -96,7 +143,7 @@ const menu = [
     title: "Chicken Burger",
     category: "Special Burgers",
     price: 6.5,
-    img: img8,
+    img: chickenBurger,
     InStock: true,
     desc: `Portland chicharrones ethical edison bulb, palo santo craft beer chia heirloom iPhone everyday`,
   },
@@ -105,7 +152,7 @@ const menu = [
     title: "Sweet Chili Chicken Burger",
     category: "Special Burgers",
     price: 6.5,
-    img: img4,
+    img: sweetChiliChickenBurger,
     InStock: true,
     desc: `Portland chicharrones ethical edison bulb, palo santo craft beer chia heirloom iPhone everyday`,
   },
@@ -114,7 +161,7 @@ const menu = [
     title: "Veggie Burger",
     category: "Special Burgers",
     price: 6.25,
-    img: img2,
+    img: veggieBurger,
     InStock: true,
     desc: `carry jianbing normcore freegan. Viral single-origin coffee live-edge, pork belly cloud bread iceland put a bird `,
   },
@@ -123,7 +170,7 @@ const menu = [
     title: "Mushroom Swiss",
     category: "Special Burgers",
     price: 6.75,
-    img: img9,
+    img: mushroomSwissBurger,
     InStock: true,
     desc: `on it tumblr kickstarter thundercats migas everyday carry squid palo santo leggings. Food truck truffaut  `,
   },
@@ -132,7 +179,7 @@ const menu = [
     title: "Chicken Finger Dinner",
     category: "Dinner box",
     price: 11.25,
-    img: img9,
+    img: chickenFingerDinner,
     InStock: true,
     desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
   },
@@ -141,7 +188,7 @@ const menu = [
     title: "Shrimp Dinner",
     category: "Dinner box",
     price: 11.95,
-    img: img1,
+    img: shrimpDinner,
     InStock: true,
     desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
   },
@@ -150,7 +197,7 @@ const menu = [
     title: "Fish and Chips",
     category: "Dinner box",
     price: 12.45,
-    img: img9,
+    img: fishAndChips,
     InStock: true,
     desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
   },
@@ -159,7 +206,7 @@ const menu = [
     title: "Regular Fries",
     category: "Side Orders",
     price: 3.5,
-    img: img2,
+    img: regularFries,
     InStock: true,
     desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
   },
@@ -168,7 +215,7 @@ const menu = [
     title: "Family Fries",
     category: "Side Orders",
     price: 9.95,
-    img: img2,
+    img: familyFries,
     InStock: true,
     desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
   },
@@ -177,7 +224,7 @@ const menu = [
     title: "Poutine",
     category: "Side Orders",
     price: 6.25,
-    img: img7,
+    img: poutine,
     InStock: true,
     desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
   },
@@ -186,7 +233,7 @@ const menu = [
     title: "Chili Cheese Fries",
     category: "Side Orders",
     price: 7.75,
-    img: img4,
+    img: chiliCheeseFries,
     InStock: true,
     desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
   },
@@ -195,7 +242,7 @@ const menu = [
     title: "Regular Onion Rings",
     category: "Side Orders",
     price: 3.5,
-    img: img6,
+    img: regularOnionRings,
     InStock: true,
     desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
   },
@@ -204,7 +251,7 @@ const menu = [
     title: "Family Onion Rings",
     category: "Side Orders",
     price: 10.25,
-    img: img6,
+    img: familyOnionRings,
     InStock: true,
     desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
   },
@@ -213,7 +260,7 @@ const menu = [
     title: "Chicken Fingers 4pcs",
     category: "Side Orders",
     price: 7.95,
-    img: img7,
+    img: chickenFingers,
     InStock: true,
     desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
   },
@@ -222,7 +269,7 @@ const menu = [
     title: "Chicken Nuggets 6pcs",
     category: "Side Orders",
     price: 5.75,
-    img: img7,
+    img: chickenNuggets,
     InStock: true,
     desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
   },
@@ -231,7 +278,7 @@ const menu = [
     title: "Chicken Nuggets 10pcs",
     category: "Side Orders",
     price: 9.45,
-    img: img7,
+    img: chickenNuggets10,
     InStock: true,
     desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
   },
@@ -240,7 +287,7 @@ const menu = [
     title: "Zucchini Sticks",
     category: "Side Orders",
     price: 5.95,
-    img: img1,
+    img: zucchiniSticks,
     InStock: true,
     desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
   },
@@ -249,16 +296,25 @@ const menu = [
     title: "Deep Fried Mushrooms",
     category: "Side Orders",
     price: 5.95,
-    img: img9,
+    img: deepFriedMushrooms,
     InStock: true,
     desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
   },
   {
     id: 27,
-    title: "Pizza pop / Corndog",
+    title: "Pizza pop",
     category: "Side Orders",
     price: 2.45,
-    img: img8,
+    img: pizzaPop,
+    InStock: true,
+    desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+  },
+  {
+    id: 27,
+    title: "Corndog",
+    category: "Side Orders",
+    price: 2.45,
+    img: cornDog,
     InStock: true,
     desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
   },
@@ -267,7 +323,7 @@ const menu = [
     title: "Apple Pie Turnover",
     category: "Side Orders",
     price: 2.45,
-    img: img5,
+    img: applePie,
     InStock: true,
     desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
   },
@@ -276,7 +332,7 @@ const menu = [
     title: "Deep Fried Pickle Stick",
     category: "Side Orders",
     price: 5.95,
-    img: img3,
+    img: deepFriedPickle,
     InStock: true,
     desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
   },
@@ -285,7 +341,7 @@ const menu = [
     title: "Gravy",
     category: "Side Orders",
     price: 1.95,
-    img: img3,
+    img: gravy,
     InStock: true,
     desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
   },
@@ -294,7 +350,7 @@ const menu = [
     title: "A cup of chili",
     category: "Side Orders",
     price: 5.95,
-    img: img3,
+    img: cupOfChili,
     InStock: true,
     desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
   },
@@ -303,7 +359,7 @@ const menu = [
     title: "Garlic Mayo",
     category: "Side Orders",
     price: 1.95,
-    img: img7,
+    img: garlicMayo,
     InStock: true,
     desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
   },
@@ -312,7 +368,7 @@ const menu = [
     title: "Hot Dog",
     category: "Hot Dogs",
     price: 3.5,
-    img: img7,
+    img: hotDog,
     InStock: true,
     desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
   },
@@ -321,7 +377,7 @@ const menu = [
     title: "Chili Dog",
     category: "Hot Dogs",
     price: 5.45,
-    img: img7,
+    img: chiliDog,
     InStock: true,
     desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
   },
@@ -330,7 +386,7 @@ const menu = [
     title: "Cheese Dog",
     category: "Hot Dogs",
     price: 4.25,
-    img: img8,
+    img: cheeseDog,
     InStock: true,
     desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
   },
@@ -339,7 +395,7 @@ const menu = [
     title: "Chili Cheese Dog",
     category: "Hot Dogs",
     price: 6.2,
-    img: img5,
+    img: chiliCheeseDog,
     InStock: true,
     desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
   },
@@ -348,7 +404,7 @@ const menu = [
     title: "Regular size POP",
     category: "Beverages",
     price: 1.95,
-    img: img1,
+    img: pop,
     InStock: true,
     desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
   },
@@ -357,7 +413,7 @@ const menu = [
     title: "Large size POP",
     category: "Beverages",
     price: 2.45,
-    img: img1,
+    img: pop,
     InStock: true,
     desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
   },
@@ -366,7 +422,7 @@ const menu = [
     title: "Regular size Slush",
     category: "Beverages",
     price: 1.95,
-    img: img1,
+    img: slush,
     InStock: true,
     desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
   },
@@ -375,7 +431,7 @@ const menu = [
     title: "Large size Slush",
     category: "Beverages",
     price: 2.45,
-    img: img1,
+    img: slush,
     InStock: true,
     desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
   },
@@ -384,7 +440,7 @@ const menu = [
     title: "Regular size Milk Shakes",
     category: "Beverages",
     price: 4.75,
-    img: img5,
+    img: milkShakes,
     InStock: true,
     desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
   },
@@ -393,7 +449,7 @@ const menu = [
     title: "Large size Milk Shakes",
     category: "Beverages",
     price: 5.5,
-    img: img5,
+    img: milkShakes,
     InStock: true,
     desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
   },
@@ -402,7 +458,7 @@ const menu = [
     title: "Float(One Size)",
     category: "Beverages",
     price: 5,
-    img: img4,
+    img: float,
     InStock: true,
     desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
   },
@@ -411,7 +467,7 @@ const menu = [
     title: "Regular size Screamers",
     category: "Beverages",
     price: 4.75,
-    img: img4,
+    img: screamers,
     InStock: true,
     desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
   },
@@ -420,7 +476,7 @@ const menu = [
     title: "Large size Screamers",
     category: "Beverages",
     price: 5.5,
-    img: img4,
+    img: screamers,
     InStock: true,
     desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
   },
@@ -429,7 +485,7 @@ const menu = [
     title: "White Milk",
     category: "Beverages",
     price: 1.75,
-    img: img2,
+    img: whiteMilk,
     InStock: true,
     desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
   },
@@ -438,16 +494,16 @@ const menu = [
     title: "Choco Milk",
     category: "Beverages",
     price: 2.75,
-    img: img2,
+    img: chocoMilk,
     InStock: true,
     desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
   },
   {
     id: 48,
-    title: "Coffee / Tea",
+    title: "Coffee",
     category: "Beverages",
     price: 2.45,
-    img: img6,
+    img: coffee,
     InStock: true,
     desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
   },
@@ -456,7 +512,7 @@ const menu = [
     title: "Hot Chocolate",
     category: "Beverages",
     price: 2.45,
-    img: img7,
+    img: hotChoco,
     InStock: true,
     desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
   },
