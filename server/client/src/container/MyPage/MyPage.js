51c588d982b198358966c0774c1877a47b2f980a
Jonghan Park <jonghan.park@edu.sait.ca>
2022-12-18 13:32:46 -0700

    Update MyPage

--- server/client/src/container/MyPage/MyPage.js
+++ server/client/src/container/MyPage/MyPage.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 import "./MyPage.css";
 
 function MyPage() {
@@ -22,14 +23,25 @@ function MyPage() {
         ) : (
           <form className="myPage-form">
             <h2>Hello, {user.name}!</h2>
-            <h3>Your Email:</h3>
-            <h4 className="userEmail">{user.email}</h4>
-            <div className="myPagePic">
-              <img src={user.pic} alt="avatar in my page" />
+            <div className="myInfo">
+              <div className="myEmail">
+                <h3>Your Email:</h3>
+                <h4 className="userEmail">{user.email}</h4>
+              </div>
+              <div className="myPagePic">
+                <img src={user.pic} alt="avatar in my page" />
+                <button type="submit" className="submit">
+                  Change your picture
+                </button>
+              </div>
+              <div className="myOrders">
+                <Link to="/viewOrders">
+                  <button type="submit" className="submit">
+                    View your orders
+                  </button>
+                </Link>
+              </div>
             </div>
-            <button type="submit" className="submit">
-              Change the profile
-            </button>
           </form>
         )}
       </div>
